567ac1c0a190ae15a6c7b94606e3a5f49c943c1d
SelinJax <seljax92@gmail.com>
2025-10-20 14:27:18 +0300

    -newtask

---
+++ easy/001-сreate-object/create-object.js
@@ -0,0 +1,16 @@
+// Завдання 1: Створення об'єкта
+
+// Створи об'єкт person з такими властивостями:
+// name (рядок)
+// age (число)
+// job (рядок)
+
+// Потім виведи значення властивості name в консоль.
+
+const person = {
+  name: "Eogeniy",
+  age: 33,
+  job: "developer",
+};
+console.table(person);
+console.log(person.name);
