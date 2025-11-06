b687e0fa25e52d4124b9c827e903b076cb880965
SelinJax <seljax92@gmail.com>
2025-11-06 17:45:00 +0200

    -newTask

---
+++ easy/005-getUserAge/getUserAge.js
@@ -0,0 +1,11 @@
+// getUserAge
+
+// Напиши функцію, яка приймає об’єкт користувача і повертає його вік.
+
+function getUserAge(user) {
+  return user.age;
+}
+
+// приклад
+const user = { name: "Іра", age: 21 };
+console.log(getUserAge(user)); // 21
