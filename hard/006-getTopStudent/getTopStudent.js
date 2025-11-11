01d90092fae9149a837504b06e7b7e4d0e456237
SelinJax <seljax92@gmail.com>
2025-11-11 03:26:18 +0200

    -newTask

--- hard/006-getTopStudent/getTopStudent.js
+++ hard/006-getTopStudent/getTopStudent.js
@@ -1,23 +1,34 @@
 // getTopStudent
 
 // Знайди студента з найвищим середнім балом.
+// function getTopStudent(students) {
+//   let topStudent = students[0];
+//   let topAverage =
+//     topStudent.grades.reduce((acc, num) => acc + num, 0) /
+//     topStudent.grades.length;
+
+//   for (const student of students) {
+//     const average =
+//       student.grades.reduce((acc, num) => acc + num, 0) / student.grades.length;
+
+//     if (average > topAverage) {
+//       topAverage = average;
+//       topStudent = student;
+//     }
+//   }
+
+//   return topStudent;
+// }
+
 function getTopStudent(students) {
-  let topStudent = students[0];
-  let topAverage =
-    topStudent.grades.reduce((acc, num) => acc + num, 0) /
-    topStudent.grades.length;
+  return students.reduce((best, current) => {
+    const bestAvg =
+      best.grades.reduce((acc, n) => acc + n, 0) / best.grades.length;
+    const currentAvg =
+      current.grades.reduce((acc, n) => acc + n, 0) / current.grades.length;
 
-  for (const student of students) {
-    const average =
-      student.grades.reduce((acc, num) => acc + num, 0) / student.grades.length;
-
-    if (average > topAverage) {
-      topAverage = average;
-      topStudent = student;
-    }
-  }
-
-  return topStudent;
+    return currentAvg > bestAvg ? current : best;
+  });
 }
 
 // приклад
